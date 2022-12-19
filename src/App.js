6d8c99e90faa1a3158c617c85e82a3b93dd07ae9
duythai2108 <thaiduy21082000@gmail.com>
2022-12-19 16:36:28 +0700

    Learn fragments + Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import "./App.scss";
+import Dropdown from "./components/Dropdown";
+import DropdownPortal from "./components/DropdownPortal";
 import SignUpFormFinal from "./components/form/SignUpFormFinal";
 import SignUpFormHook from "./components/form/SignUpFormHook";
+import Modal from "./components/modal/Modal";
+import Game from "./components/tictactoe/Game";
+import Tooltip from "./components/Tooltip";
+import { ErrorBoundary } from "react-error-boundary";
+
 // import Form from "./components/form/Form";
 // import SignUpForm from "./components/form/SignUpForm";
 // import SignUpFormV2 from "./components/form/SignUpFormV2";
@@ -39,11 +46,45 @@ import SignUpFormHook from "./components/form/SignUpFormHook";
 // ES6
 // Babel
 // parent component
-function App() {
+function ErrorFallback({ error, resetErrorBoundary }) {
   return (
-    <div>
-      <SignUpFormHook></SignUpFormHook>
+    <div role="alert" className="p-5 bg-red-100">
+      <p className="text-red-600">Khong the lay data do component dang loi</p>
+      <button onClick={resetErrorBoundary}>Try again</button>
     </div>
+  );
+}
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <Fragment>
+      {/* <div className="relaive z-0">
+        <Modal open={showModal} handleClose={() => setShowModal(false)}></Modal>
+      </div>
+      <button
+        className="p-4 bg-blue-500 text-white rounded-lg"
+        onClick={() => setShowModal(true)}
+      >
+        Show modal
+      </button>
+      <div className="relative z-30">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
+        aspernatur odio nesciunt a alias aliquam consequatur quos. Ex harum
+        laboriosam, cupiditate doloremque consequuntur culpa, tenetur alias sunt
+        dolore cumque dolores.
+      </div>
+      <div className="overflow-hidden p-5">
+        <DropdownPortal></DropdownPortal>
+      </div> */}
+      {/* <div className="p-16 mt-16 ml-16">
+        <Tooltip text="Hover me">This is a tooltip content</Tooltip>
+      </div> */}
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Game></Game>
+      </ErrorBoundary>
+      <SignUpFormFinal></SignUpFormFinal>
+    </Fragment>
   );
 }
 export default App;
